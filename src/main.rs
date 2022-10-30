35c016db395715ea04d902553bcca5505801e0fb
Richard <richard.w.morton86@gmail.com>
2022-10-30 19:24:46 +0200

    added SDL2 window

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,7 @@
-mod math;
-use math::vector::Vector;
-use math::point::Point;
-use math::ray::Ray;
-
-mod film;
-mod sandbox;
+mod view;
+use view::window::Window;
 
 fn main() {
-    // sandbox::img::gen_fractal();
-
-    sandbox::mem::test1(&3.4);
-    sandbox::mem::test2();
-    println!("\n-----------------\n");
-    sandbox::data::test1();
-    println!("\n-----------------\n");
-    sandbox::closure::test1();
-    sandbox::closure::test2();
-    sandbox::closure::test3();
-    println!("\n-----------------\n");
-    sandbox::str::test1();
-    sandbox::str::test2();
-    println!("\n-----------------\n");
-    sandbox::ranges::test1();
-    println!("\n-----------------\n");
-    sandbox::iter::test1();
-    println!("\n-----------------\n");
-    sandbox::err::test1();
-    sandbox::err::test2();
-    println!("\n-----------------\n");
-    sandbox::io::test1();
-
-    // test display trait
-    let v = math::vector::Vector::new(1.,2.,3.);
-    println!("vector = {}",v);
-    let o = Point::new(1.,2.,3.);
-    let d = Vector::new(-3.,-1.,2.);
-    let r = Ray::new(&o,&d);
-    println!("ray = {}",r);
+    let window: Window = Window::new(800,600);
+    window.run().unwrap();
 }
---
+++ src/view/window.rs
@@ -0,0 +1,88 @@
+use sdl2::{Sdl,VideoSubsystem};
+use sdl2::video;
+use sdl2::render::{Canvas,Texture};
+use sdl2::pixels::Color;
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+
+use std::time::Duration;
+
+static WINDOW_TITLE: &str = "Rust Raytracing Demo";
+
+#[allow(dead_code)]
+pub struct Window {
+    width: u32,
+    height: u32,
+    context: Sdl,
+    video: VideoSubsystem,
+    canvas: Canvas<video::Window>,
+    //
+    // TEMP
+    i: u8
+}
+
+impl Window {
+    /// Construct window
+    pub fn new(width: u32,height: u32) -> Window {
+        //
+        let context = sdl2::init().unwrap();
+        let video = context.video().unwrap();
+        let window: video::Window = video
+            .window(WINDOW_TITLE,width,height)
+            .position_centered()
+            .build()
+            .expect("Failed to initialize SDL2 video subsystem");
+        let canvas = window
+            .into_canvas()
+            .build()
+            .expect("Failed to make SDL2 canvas");
+
+        Window {
+            width,height,
+            context,video,canvas,
+            //
+            // TEMP
+            i: 0
+        }
+    }
+
+    /// Main loop
+    pub fn run(mut self) -> Result<(),String> {
+        let mut event_pump = self.context.event_pump()?;
+        'running: loop {
+            for event in event_pump.poll_iter() {
+                match event {
+                    Event::Quit {..} | Event::KeyDown {
+                        keycode: Some(Keycode::Escape),..
+                    } => {
+                        break 'running;
+                    },
+                    _ => {}
+                }
+            }
+
+            self.update();
+            self.render();
+
+            ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
+        }
+
+        Ok(())
+    }
+
+    /// Update
+    fn update(&mut self) {
+        //
+        // TEMP
+        self.i = (self.i + 1) % 255;
+    }
+
+    /// Render
+    fn render(&mut self) {
+        //
+        // TEMP
+        self.canvas.set_draw_color(Color::RGB(self.i, 64, 255 - self.i));
+        self.canvas.clear();
+        self.canvas.present();
+    }
+}
